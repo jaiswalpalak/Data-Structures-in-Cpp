8237790eb9ac8e98bcecc3369e533c827f028186
Akash Yadav <78416508+4kashyadav@users.noreply.github.com>
2022-07-10 15:46:00 +0530

    Create Running Sum of 1d Array.cpp

---
+++ LEET CODE/Running Sum of 1d Array.cpp
@@ -0,0 +1,9 @@
+vector<int> runningSum(vector<int>& nums) {
+        int sum = 0;
+        vector<int> ans;
+        for(int i =0; i<nums.size();i++){
+            sum += nums[i];
+            ans.push_back(sum);
+        }
+        return ans;
+    }
