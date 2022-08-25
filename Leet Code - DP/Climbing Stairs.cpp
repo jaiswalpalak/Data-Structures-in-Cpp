9a4f9221a49e75f751608fd6b0a0cdadf4771037
Akash Yadav <78416508+4kashyadav@users.noreply.github.com>
2022-08-26 02:26:05 +0530

    Create Climbing Stairs.cpp

---
+++ Leet Code - DP/Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+//Understand a DP Problem
+
+//Count the total no. of ways ( Min & Max)
+
+int climbStairs(int n) {
+        int *arr = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 1;
+        
+        for(int i =2; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
+    }
