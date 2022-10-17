5393c238381dbf612f6e86d6928050637950d62b
Akash Yadav <78416508+4kashyadav@users.noreply.github.com>
2022-10-17 23:44:45 +0530

    Create Zigzag Conversion.cpp

---
+++ LEET CODE - String/Zigzag Conversion.cpp
@@ -0,0 +1,36 @@
+/*n=numRows
+Δ=2n-2    1                           2n-1                         4n-3
+Δ=        2                     2n-2  2n                    4n-4   4n-2
+Δ=        3               2n-3        2n+1              4n-5       .
+Δ=        .           .               .               .            .
+Δ=        .       n+2                 .           3n               .
+Δ=        n-1 n+1                     3n-3    3n-1                 5n-5
+Δ=2n-2    n                           3n-2                         5n-4
+*/
+
+
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<string> v(numRows, "");
+        int i =0;
+        int n = s.length();
+        while(i<n){
+            
+            for(int j =0; j<numRows && i<n; j++ ){
+                v[j].push_back(s[i++]);
+            }
+            
+            for(int j =numRows-2; j>=1 && i<n; j--){
+                v[j].push_back(s[i++]);
+            }
+        }
+        
+        string res ="";
+        for(auto i : v){
+            res+=i;
+        }
+        return res;
+    }
+};
